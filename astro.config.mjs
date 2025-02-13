340fa10a0b6f8bfbfe091cd49f99eb3a351c54e6
zwingthomas <zwingthomas@gmail.com>
2025-02-12 18:58:33 -0600

    Fixed Astro integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 import svelte from '@astrojs/svelte';
 
 export default defineConfig({
-  integrations: [svelte()],
+  integrations: [
+    svelte(),
+    tailwind()
+  ],
 });
