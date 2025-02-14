168b82b2db06693667c021abf84ebc48fe608e2a
jt <jt@modernbanc.com>
2025-02-14 08:41:31 -0600

    feat: got grid populated

--- src/components/FilmCard.svelte
+++ src/components/FilmCard.svelte
@@ -1,17 +1,18 @@
 <script lang="ts">
   import type { Film } from '../types';
   export let film: Film;
+  import Image from 'astro:assets';
 </script>
 
-<div class="card w-72 bg-neutral shadow-xl hover:scale-105 transition-all">
+<div class="card w-full max-w-72">
   <figure>
-    <img src={film.posterUrl} alt={film.title} class="rounded-t-lg" />
+    <Image src={film.posterUrl} alt={`Poster for ${film.title}`} />
   </figure>
-  <div class="card-body">
-    <h2 class="card-title">{film.title}</h2>
-    <p>{film.genre}</p>
+  <div class="">
+    <h2 class="text-sm text-center truncate max-w-100">{film.title}</h2>
+    <!-- <p>{film.genre}</p>
     <div class="card-actions justify-end">
       <a href={`/films/${film.id}`} class="btn btn-outline">Watch</a>
-    </div>
-  </div>
+    </div> -->
+  </div> 
 </div>
