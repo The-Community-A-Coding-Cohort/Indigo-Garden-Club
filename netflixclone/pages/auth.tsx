2d78fa67cb9bb91cb03cd78c5ac68c9d6347694e
pyericl <pyericl@gmail.com>
2025-03-05 19:03:15 -0600

    set up and pending changes for image

---
+++ netflixclone/pages/auth.tsx
@@ -0,0 +1,11 @@
+// import hero from '../public/images/hero.webp'
+const Auth = () => {
+    return (
+        <div className="relative h-full w-full bg-[url('/images/bigRedTest.jpg')] bg-no-repeat bg-center bg-fixed"> 
+        </div>
+        // <div className="hero"> {{hero}} </div>
+        // <div className="hero"/>
+        // <div> default </div>
+    )
+}
+export default Auth;
---
+++ netflixclone/pages/test.tsx
@@ -0,0 +1,6 @@
+const MyPage = () => {
+    return (
+        <div> Testing testing 123 </div>
+    )
+}
+export default MyPage;
