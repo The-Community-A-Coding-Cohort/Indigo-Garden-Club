f9a1f5d5c80d3edd9cd3c036884fc33d2ffb4a68
pyericl <pyericl@gmail.com>
2025-03-07 17:59:19 -0600

    added Navbar, Billboard and partial AccountMenu

--- netflixclone/components/Navbar.tsx
+++ netflixclone/components/Navbar.tsx
@@ -1,17 +1,42 @@
 import NavbarItem from './NavbarItem';
 import MobileMenu from './MobileMenu';
+import AccountMenu from './AccountMenu';
 import { BsBell, BsChevronDown, BsSearch } from 'react-icons/bs';
-import { useCallback, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
+
+const TOP_OFFSET = 66;
 
 const Navbar = () => {
     const [showMobileMenu, setShowMobileMenu] = useState(false);
     const toggleMobileMenu = useCallback(() => {
         setShowMobileMenu((current) => !current);
     }, [])
+    
+    const [showAccountMenu, setShowAccountMenu] = useState(false);
+    const toggleAccountMenu = useCallback(() => {
+        setShowAccountMenu((current) => !current);
+    }, [])
+
+    const [showBackground, setShowBackground] = useState(false);
+    useEffect(() => {
+        const handleScroll = () => {
+            if(window.scrollY >= TOP_OFFSET){
+                setShowBackground(true);
+            } else {
+                setShowBackground(false);
+            }
+        }
+
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        }
+    })
 
     return (
         <div className="w-full fixed z-40">
-            <div className="px-4 
+            <div className={`
+            px-4 
             md:px-16 
             py-6 
             flex 
@@ -19,8 +44,8 @@ const Navbar = () => {
             items-center 
             transition 
             duration-500 
-            bg-zinc-900 
-            bg-opacity-90">
+            ${ showBackground ? 'bg-zinc-900 bg-opacity-90' : ''}
+            `}>
                 <img className="h-4 lg:h-7" src="/images/netflixLogo.png" alt="logo"/>
                 <div className="flex-row ml-8 gap-7 hidden lg:flex">
                 <NavbarItem label='Home'/>
@@ -33,7 +58,7 @@ const Navbar = () => {
             </div>
             <div onClick={toggleMobileMenu} className='lg:hidden flex flex-row items-center gap-2 ml-8 cursor-pointer relative'>
                 <p className='text-white text-sm'> Browse </p>
-                <BsChevronDown className='text-white transition'/>
+                <BsChevronDown className={`text-white transition ${showAccountMenu ? 'rotate-180' : 'rotate-0'}`}/>
                 <MobileMenu visible={showMobileMenu}/>
             </div>
             <div className='flex flex-row ml-auto gap-7 items-center'>
@@ -45,10 +70,12 @@ const Navbar = () => {
                 </div>
             </div>
 
-            <div className='flex flex-row items-center gap-2 cursor-pointer relative'>
+            <div onClick={toggleAccountMenu} className='flex flex-row items-center gap-2 cursor-pointer relative'>
                 <div className='w-6 h-6 lg:w-10 lg:h-10 rounded-md overflow-hidden'>
-                    //
+                    <img src='/images/blueDefaultGuy.jpg' alt=''/>
                 </div>
+                <BsChevronDown className={`text-white transition ${showAccountMenu ? 'rotate-180' : 'rotate-0'}`}/>
+                <AccountMenu visible={showAccountMenu}/>
             </div>
         </div>
     )
--- netflixclone/pages/index.tsx
+++ netflixclone/pages/index.tsx
@@ -2,29 +2,38 @@ import {getSession, signOut} from 'next-auth/react';
 import {NextPageContext} from 'next';
 import useCurrentUser from '../hooks/useCurrentUser';
 import Navbar from '../components/Navbar';
+import Billboard from '../components/Billboard';
+import MovieList from '../components/MovieList'
 
-// export async function getServerSideProps(context: NextPageContext) {
-//   const session = await getSession(context);
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
 
-//   if (!session) {
-//     return {
-//       redirect: {
-//         destination: '/auth',
-//         permanent: false
-//       }
-//     }
-//   }
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/auth',
+        permanent: false
+      }
+    }
+  }
 
-//   return {
-//     props: {}
-//   }
-// }
+  return {
+    props: {}
+  }
+}
 
 export default function Home() {
   const { data: user } = useCurrentUser();
+  const movieData = [
+    {title: "hello world", item: []},
+  ]
   return (
     <>
       <Navbar />
+      <Billboard />
+      <div className='pb-48'>
+        <MovieList title='Trending Now' data={movieData}/>
+      </div>
     </>
   )
 }
