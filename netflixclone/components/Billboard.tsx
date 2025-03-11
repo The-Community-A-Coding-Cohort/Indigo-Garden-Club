ceea340785dd3547e1c1b5c733d8f95a936854fd
zwingthomas <zwingthomas@gmail.com>
2025-03-11 12:36:35 -0500

    Billboard button styling

--- netflixclone/components/Billboard.tsx
+++ netflixclone/components/Billboard.tsx
@@ -22,7 +22,7 @@ const Billboard = () => {
                 </p>
                 <div className="flex flex-row items-center mt-3 md:mt-4 gap-3">
                     <PlayButton movieId={data?.movieId}/>
-                    <button className="bg-white text-white bg-opacity-30 rounded-md py-1 md:py-2 px-2 md:px-4 w-auto text-xs lg:text-lg font-semibold flex flex-row items-center hover:bg-opacity-20 transition">
+                    <button className="bg-white/30 text-white rounded-md py-1 md:py-2 px-2 md:px-4 w-auto text-xs lg:text-lg font-semibold flex flex-row items-center hover:bg-opacity-20 hover:bg-neutral-600 transition">
                         <AiOutlineInfoCircle className="mr-1"/>
                         More Info
                     </button>
