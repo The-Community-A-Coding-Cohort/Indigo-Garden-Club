7d96ed833b597d2f39744355b3114f45b8579da7
pyericl <pyericl@gmail.com>
2025-03-19 19:02:04 -0500

    debug statements for favorite button Co-authored-by: zwingthomas zwingthomas@gmail.com

--- netflixclone/lib/serverAuth.ts
+++ netflixclone/lib/serverAuth.ts
@@ -4,7 +4,16 @@ import { getSession } from 'next-auth/react';
 import prismadb from './prismadb';
 
 const serverAuth = async (req: NextApiRequest) => {
+    
+    // console.log(`in serverAuth requests begin`)
+    // if(req.method == 'POST'){
+    //     console.log(req);
+    // }
+    // console.log(`in serverAuth requests end`)
     const session = await getSession( {req} );
+    console.log(`serverAuth.ts call from ${req.url} : ${req.method} : throw error?: ${!session?.user?.email}`)
+    // console.log(`in serverAuth throw error?: ${!session?.user?.email}`);
+    // console.log(session);
 
     if (!session?.user?.email) {
         throw new Error('Not signed in');
