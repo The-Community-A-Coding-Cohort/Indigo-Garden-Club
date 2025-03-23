d9959c215ba9e09f0c4a7a8f29a5345840685413
zwingthomas <zwingthomas@gmail.com>
2025-03-23 17:17:48 -0500

    Mistake with the acm cert

--- terraform/netflixclone/acm_cert.tf
+++ terraform/netflixclone/acm_cert.tf
@@ -8,7 +8,30 @@ resource "aws_acm_certificate" "cert" {
   }
 }
 
+resource "aws_route53_record" "cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      type   = dvo.resource_record_type
+      record = dvo.resource_record_value
+    }
+  }
+  zone_id = var.hosted_zone_id
+  name    = each.value.name
+  type    = each.value.type
+  records = [each.value.record]
+  ttl     = 60
+
+  lifecycle {
+    ignore_changes = [
+      records,
+      name,
+      type,
+    ]
+  }
+}
+
 resource "aws_acm_certificate_validation" "cert" {
   certificate_arn         = aws_acm_certificate.cert.arn
-  validation_record_fqdns = [var.validation_record_fqdns]
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
 }
