4b36078f305bfb192e5849e5838fcd8b7f6459cc
pyericl <pyericl@gmail.com>
2025-04-02 18:28:33 -0500

    added jest and jest.config for running unit tests Co-authored-by: zwingthomas zwingthomas@gmail.com

---
+++ netflixclone/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    testEnvironment: "node",
+    testMatch: [
+    //   "<rootDir>/src/**/*.test.ts"  // Match any test files in the "src" folder ending in ".test.js"
+        "<rootDir>/**/**/*.test.ts"  // Match any test files in the "src" folder ending in ".test.js"
+    ],
+    // You can also use testRegex to define patterns
+    // testRegex: "/tests/.*\\.test\\.js$",
+  };
