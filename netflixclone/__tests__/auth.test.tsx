281d848e0d9adc224123c4380cd984ea0cd9bfca
pyericl <pyericl@gmail.com>
2025-04-05 18:19:06 -0500

    fixed unit test getByText
    able to find sign in text now
    
    Co-Authored-By: LeticeZwinger <160505543+LeticeZwinger@users.noreply.github.com>
    Co-Authored-By: Thomas Zwinger <zwingthomas@gmail.com>

--- netflixclone/__tests__/auth.test.tsx
+++ netflixclone/__tests__/auth.test.tsx
@@ -1,23 +1,26 @@
 import React from 'react';
-import {describe, expect, test, beforeAll} from '@jest/globals';
+import {describe, expect, test, beforeAll, beforeEach} from '@jest/globals';
 import { render, screen } from '@testing-library/react'; 
 import Auth from '../pages/auth';
 
 describe('testing auth.tsx: ', () => {
-    // beforeAll(() => {
-    //     render(<Auth />);
-    // })
-    test('should render', () => {
+    beforeEach(() => {
         render(<Auth />);
+    })
+    test('should render', () => {
+        // render(<Auth />);
         // Auth()
         
         const titleElement = screen.getByRole('heading', { level: 2 });
+        const logoElement = screen.findByAltText('Logo');
         expect(titleElement.textContent).toBe('Sign in');
-        // expect(titleElement).toBeInTheDocument();
+        expect(logoElement).toBeDefined();
+        // expect(logoElement).toBeInTheDocument();
     });
-    test('on click', () => {
-        const loginVariantButton = screen.getByText('test element');
-        console.log(loginVariantButton.textContent);
+    test('should start out as Sign in', async () => {
+        const loginVariantButton = await screen.getByText('Sign in');
+        // const loginVariantButton = screen.findByAltText('Logo');
+        // console.log(loginVariantButton.textContent);
         expect(loginVariantButton).toBeDefined();
 
     });
